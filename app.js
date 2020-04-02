53b3630b28d3fdd5d72847db0659be1e064721be
Tanishq <jtanishq99@gmail.com>
2020-04-02 15:44:20 +0530

    second commit md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+var md5=require("md5");
 
 
 
@@ -23,9 +23,8 @@ const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
-
-var secret=process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret ,encryptedFields:["password"]});
+var v=md5("anikota1");
+console.log(v);
 
 const User=new mongoose.model("User",userSchema);
 
@@ -44,7 +43,7 @@ app.get("/login", function(req, res){
 app.post("/register",function(req,res){
   const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -59,7 +58,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   var email=req.body.username;
-  var password=req.body.password;
+  var password=md5(req.body.password);
 
   User.findOne({email:email},function(err,foundUser){
     if(err){
